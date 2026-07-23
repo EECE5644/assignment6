a86cf7d2f16b2cdce14f6da146b8d906eff6ffa3
dianaHyun <dhyun829@gmail.com>
2026-07-22 23:33:54 -0400

    Worked little on 5

---
+++ 5.py
@@ -0,0 +1,24 @@
+# Anomaly Alerting
+residuals = total_actuals.flatten() - total_preds.flatten()
+
+sigma = residuals.std()
+threshold = 3 * sigma
+
+anomalies = np.abs(residuals) > threshold
+print(anomalies)
+
+# Check if np.abs(residuals) > threshold continue for more than 2 consecutive hours and gather only the true case
+alert_indices = []
+count = 0
+for i, flag in enumerate(anomalies):
+    if flag:
+      count += 1
+      if count >= 2:
+        alert_indices.append(i)
+    else:
+      count = 0
+
+print("Residuals:", residuals)
+print("Threshold:", threshold)
+print("Anomalies:", anomalies)
+print("Alert indices (2+ consecutive anomalies):", alert_indices)
